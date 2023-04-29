d50f2687994a9a0c1569feb508e42e4da7a4d55f
nweke joseph <nwekejoseph52@gmail.com>
2023-04-30 00:44:02 +0100

    changed property name of type EvalResult to resultStr

--- src/app/functions/eval.ts
+++ src/app/functions/eval.ts
@@ -1,25 +1,28 @@
 //Evaluate when the any operator button is pressed.
-type EvaluateResult = {
-  result: string | number | null;
-  resultForUI: string | null;
+export type EvalResult = {
+  resultStr: string;
+  resultForUI: string;
 };
 
-export const evaluate = (
-  operation: string
-): EvaluateResult | Error | undefined => {
-  if (!/^((\d+?[.]\d+?)|\d+)(?:[+*-/])((\d+?[.]\d+?)|\d+)$/.test(operation)) {
-    return;
+export const evaluate = (operation: string): EvalResult | Error | undefined => {
+  if (
+    !/^([-])?((\d+?[.]\d+?)|\d+)([+*-/])([-])?((\d+?[.]\d+?)|\d+)$/.test(
+      operation
+    )
+  ) {
+    return undefined as undefined;
   }
 
-  const result: string | number = eval(operation);
+  const result = eval(operation);
   const resultForUI = Number(eval(operation)).toLocaleString();
 
   // Handle edge cases
   try {
     if (!isFinite(result as number)) throw Error;
   } catch (e) {
-    throw e;
+    throw e as Error;
   }
 
-  return { result, resultForUI };
+  const resultStr = String(result);
+  return { resultStr, resultForUI } as EvalResult;
 };
